2415505d048b43a6c35d7748bd8d7e30a38065e2
wsybok <wsybok@gmail.com>
2024-07-30 13:20:00 +0000

    fixed bug in app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from crewai import Agent, Task, Process, Crew
 from langchain_community.agent_toolkits.load_tools import load_tools
-from agents import StockAnalysisAgents
-from tasks import StockAnalysisTasks
+from agents import explorer, writer, critic
+from tasks import task_report, task_blog, task_critique
 
 api = os.environ.get("OPENAI_API_KEY")
 
