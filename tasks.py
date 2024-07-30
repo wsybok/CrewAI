7725ef4c8bc6d01da13908129d26d6cf32a637d6
wsybok <wsybok@gmail.com>
2024-07-30 17:36:22 +0800

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,5 +1,10 @@
-task_report = Task(
-    description="""Use and summarize scraped data from subreddit LocalLLama to make a detailed report on the latest rising projects in AI. Use ONLY scraped data from LocalLLama to generate the report. Your final answer MUST be a full analysis report, text only, ignore any code or anything that isn't text. The report has to have bullet points and with 5-10 exciting new AI projects and tools. Write names of every tool and project. Each bullet point MUST contain 3 sentences that refer to one specific AI company, product, model or anything you found on subreddit LocalLLama.""",
+from crewai import Task
+from textwrap import dedent
+
+class CrewTasks():
+    def task_report():
+      return Task(description=dedent(f"""
+      Use and summarize scraped data from subreddit LocalLLama to make a detailed report on the latest rising projects in AI. Use ONLY scraped data from LocalLLama to generate the report. Your final answer MUST be a full analysis report, text only, ignore any code or anything that isn't text. The report has to have bullet points and with 5-10 exciting new AI projects and tools. Write names of every tool and project. Each bullet point MUST contain 3 sentences that refer to one specific AI company, product, model or anything you found on subreddit LocalLLama.""",
     agent=explorer,
     expected_output="""A text-based analysis report with the following structure:
     - Title of the report
@@ -11,9 +16,9 @@ task_report = Task(
     - Conclusion
     The report should be detailed and concise, summarizing the most exciting new AI projects and tools found on the LocalLLama subreddit."""
 )
-
-task_blog = Task(
-    description="""Write a blog article with text only and with a short but impactful headline and at least 10 paragraphs. The blog should summarize
+    def task_blog():
+      return Task(description=dedent(f"""
+      Write a blog article with text only and with a short but impactful headline and at least 10 paragraphs. The blog should summarize
     the report on the latest AI tools found on the LocalLLama subreddit. The style and tone should be compelling and concise, fun, technical but also use
     layman words for the general public. Name specific new, exciting projects, apps, and companies in the AI world. Don't
     write "**Paragraph [number of the paragraph]:**", instead start the new paragraph in a new line. Write the names of projects and tools in BOLD.
@@ -40,20 +45,20 @@ task_blog = Task(
     - Own thoughts on how it connects to the overall theme of the newsletter
     ```"""
 )
-
-task_critique = Task(
-    description="""The task entails summarizing and critiquing AI projects from the LocalLLama subreddit. The output must be formatted in Markdown and include interesting facts and personal thoughts on how each project connects to the overall theme of the newsletter.""",
-    agent=critic,
-    output_file='output.md',
-    expected_output="""The Output MUST have the following markdown format:
-    ```
-    ## [Title of post](link to post)
-    - Interesting facts
-    - Own thoughts on how it connects to the overall theme of the newsletter
-    ## [Title of second post](link to post)
-    - Interesting facts
-    - Own thoughts on how it connects to the overall theme of the newsletter
-    ```
-    The critique should be compelling and concise, with explanations in layman terms, summarizing AI projects from the LocalLLama subreddit, with sections for interesting facts, key features, and potential impacts."""
+      
+    def task_critique():
+      return Task(description=dedent(f"""
+      The task entails summarizing and critiquing AI projects from the LocalLLama subreddit. The output must be formatted in Markdown and include interesting facts and personal thoughts on how each project connects to the overall theme of the newsletter.""",
+      agent=critic,
+      output_file='output.md',
+      expected_output="""The Output MUST have the following markdown format:
+      ```
+      ## [Title of post](link to post)
+      - Interesting facts
+      - Own thoughts on how it connects to the overall theme of the newsletter
+      ## [Title of second post](link to post)
+      - Interesting facts
+      - Own thoughts on how it connects to the overall theme of the newsletter
+      ```
+      The critique should be compelling and concise, with explanations in layman terms, summarizing AI projects from the LocalLLama subreddit, with sections for interesting facts, key features, and potential impacts."""
 )
-
