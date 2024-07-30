8c117c6a5a72f8089ba5bfb8d9c52beb23654154
wsybok <wsybok@gmail.com>
2024-07-30 13:59:01 +0000

    fixed

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ from tasks import CrewTasks
 api = os.environ.get("OPENAI_API_KEY")
 
 
+agents = CrewAgents()
+tasks = CrewTasks()
+
 crew = Crew(
-    agents=[explorer, writer, critic],
-    tasks=[task_report, task_blog, task_critique],
+    agents=[agents.explorer(), agents.writer(), agents.critic()],
+    tasks=[tasks.task_report(), tasks.task_blog(), tasks.task_critique()],
     verbose=2,
     process=Process.sequential,
 )
